c35e4276e05541c6cb369406483a2b2a25dfa5a4
Simon Melchertsen <warcraft.simon@gmail.com>
2018-11-27 12:48:47 +0100

    Mp3 tag

--- src/mytunes/MyTunes.java
+++ src/mytunes/MyTunes.java
@@ -5,12 +5,22 @@
  */
 package mytunes;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import mytunes.be.Song;
+import org.farng.mp3.MP3File;
+import org.farng.mp3.TagException;
+import org.farng.mp3.id3.AbstractID3v2;
 
 /**
  *
@@ -18,14 +28,14 @@ import mytunes.be.Song;
  */
 public class MyTunes extends Application
 {
-    
+
     @Override
     public void start(Stage stage) throws Exception
     {
         Parent root = FXMLLoader.load(getClass().getResource("gui/FXMLDocument.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
         stage.show();
     }
@@ -33,11 +43,27 @@ public class MyTunes extends Application
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException, TagException
     {
-        launch(args);
-        
-     
+        //launch(args);
+        File sourceFile = new File("Data/Blue_Dot_Sessions_-_04_-_Cupcake_Marshall.mp3");
+        MP3File mp3file = new MP3File(sourceFile);
+        if (mp3file.hasID3v2Tag())
+        {
+            AbstractID3v2 tag = mp3file.getID3v2Tag();
+            int albumTitle = tag.getSize();
+            System.out.println(albumTitle);
+            return;
+        }
+        else 
+        {
+            System.out.println("fuck");
+        }
     }
-    
+
+    public static void readID3() throws FileNotFoundException, IOException
+    {
+        
+    }
+
 }
