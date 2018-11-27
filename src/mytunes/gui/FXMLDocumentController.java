3044f1ebb90bbb3e3e009ede696c4d651c9d60a7
rich0285 <44639744+rich0285@users.noreply.github.com>
2018-11-27 10:51:44 +0100

    Gui med fxid og buttonaction

--- src/mytunes/gui/FXMLDocumentController.java
+++ src/mytunes/gui/FXMLDocumentController.java
@@ -19,6 +19,8 @@ public class FXMLDocumentController implements Initializable
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb)
