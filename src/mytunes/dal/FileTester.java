098875b7c671b97f6cb9c78654e2d6bf868d995b
Simon Melchertsen <warcraft.simon@gmail.com>
2018-11-28 10:24:58 +0100

    update to songtodb

--- src/mytunes/dal/FileTester.java
+++ src/mytunes/dal/FileTester.java
@@ -10,6 +10,7 @@ import com.mpatric.mp3agic.ID3v2;
 import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.Mp3File;
 import com.mpatric.mp3agic.UnsupportedTagException;
+import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 import mytunes.be.Song;
@@ -25,6 +26,7 @@ public class FileTester
     {
         SongDAO songDAO = new SongDAO();
         mp3toDB mp3todb = new mp3toDB();
+        mp3todb.mp3songToDBTable(new File("Data/BenJamin_Banger_-_01_-_Bobby_Drake.mp3"));
 //        songDAO.createSong("TestSong", "TestArtist", 150, "TestType");
 //        songDAO.deleteSong(new Song(2, "", "", 3, ""));
 //        songDAO.updateSong(new Song(3,"TESTUPDATE","ARTISTUPDATE",999,"UPDATED"));
@@ -35,36 +37,36 @@ public class FileTester
 //        songDAO.getSong(3);
 
 //        mp3todb.mp3songToDBTable(new Mp3File("Data/BenJamin_Banger_-_01_-_Bobby_Drake.mp3"));
-        Mp3File mp3file = new Mp3File("Data/BenJamin_Banger_-_01_-_Bobby_Drake.mp3");
-        System.out.println("Length: " + mp3file.getLengthInSeconds());
-        System.out.println("" + (mp3file.hasId3v1Tag() ? "YES" : "NO"));
-        System.out.println("" + (mp3file.hasId3v2Tag() ? "YES" : "NO"));
-        if (mp3file.hasId3v2Tag())
-        {
-            ID3v2 id3v2Tag = mp3file.getId3v2Tag();
-            System.out.println("Track: " + id3v2Tag.getTrack());
-            System.out.println("Artist: " + id3v2Tag.getArtist());
-            System.out.println("Title: " + id3v2Tag.getTitle());
-            System.out.println("Album: " + id3v2Tag.getAlbum());
-            System.out.println("Year: " + id3v2Tag.getYear());
-            System.out.println("Genre: " + id3v2Tag.getGenre() + " (" + id3v2Tag.getGenreDescription() + ")");
-            System.out.println("Comment: " + id3v2Tag.getComment());
-            System.out.println("Lyrics: " + id3v2Tag.getLyrics());
-            System.out.println("Composer: " + id3v2Tag.getComposer());
-            System.out.println("Publisher: " + id3v2Tag.getPublisher());
-            System.out.println("Original artist: " + id3v2Tag.getOriginalArtist());
-            System.out.println("Album artist: " + id3v2Tag.getAlbumArtist());
-            System.out.println("Copyright: " + id3v2Tag.getCopyright());
-            System.out.println("URL: " + id3v2Tag.getUrl());
-            System.out.println("Encoder: " + id3v2Tag.getEncoder());
-            byte[] albumImageData = id3v2Tag.getAlbumImage();
-            if (albumImageData != null)
-            {
-                System.out.println("Have album image data, length: " + albumImageData.length + " bytes");
-                System.out.println("Album image mime type: " + id3v2Tag.getAlbumImageMimeType());
-            }
-        }
-
+//        Mp3File mp3file = new Mp3File("Data/BenJamin_Banger_-_01_-_Bobby_Drake.mp3");
+//        System.out.println("Length: " + mp3file.getLengthInSeconds());
+//        System.out.println("" + (mp3file.hasId3v1Tag() ? "YES" : "NO"));
+//        System.out.println("" + (mp3file.hasId3v2Tag() ? "YES" : "NO"));
+//        if (mp3file.hasId3v2Tag())
+//        {
+//            ID3v2 id3v2Tag = mp3file.getId3v2Tag();
+//            System.out.println("Track: " + id3v2Tag.getTrack());
+//            System.out.println("Artist: " + id3v2Tag.getArtist());
+//            System.out.println("Title: " + id3v2Tag.getTitle());
+//            System.out.println("Album: " + id3v2Tag.getAlbum());
+//            System.out.println("Year: " + id3v2Tag.getYear());
+//            System.out.println("Genre: " + id3v2Tag.getGenre() + " (" + id3v2Tag.getGenreDescription() + ")");
+//            System.out.println("Comment: " + id3v2Tag.getComment());
+//            System.out.println("Lyrics: " + id3v2Tag.getLyrics());
+//            System.out.println("Composer: " + id3v2Tag.getComposer());
+//            System.out.println("Publisher: " + id3v2Tag.getPublisher());
+//            System.out.println("Original artist: " + id3v2Tag.getOriginalArtist());
+//            System.out.println("Album artist: " + id3v2Tag.getAlbumArtist());
+//            System.out.println("Copyright: " + id3v2Tag.getCopyright());
+//            System.out.println("URL: " + id3v2Tag.getUrl());
+//            System.out.println("Encoder: " + id3v2Tag.getEncoder());
+//            byte[] albumImageData = id3v2Tag.getAlbumImage();
+//            if (albumImageData != null)
+//            {
+//                System.out.println("Have album image data, length: " + albumImageData.length + " bytes");
+//                System.out.println("Album image mime type: " + id3v2Tag.getAlbumImageMimeType());
+//            }
+//        }
+//
     }
 
 }
--- src/mytunes/dal/mp3toDB.java
+++ src/mytunes/dal/mp3toDB.java
@@ -6,7 +6,11 @@
 package mytunes.dal;
 
 import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException;
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -26,9 +30,10 @@ public class mp3toDB {
         dbConnect = new DBConnectionProvider();
     }
 
-    public Song mp3songToDBTable(Mp3File mp3file) {
+    public Song mp3songToDBTable(File file) throws IOException, UnsupportedTagException, InvalidDataException {
 
-        String sql = "INSERT INTO Song(title, artist, time, category) VALUES(?,?,?,?);";
+        Mp3File mp3file = new Mp3File(file.getAbsoluteFile());
+        String sql = "INSERT INTO Song(title, artist, time, category, filePath) VALUES(?,?,?,?,?);";
         ID3v2 id3v2Tag = mp3file.getId3v2Tag();
         
         try (Connection con = dbConnect.getConnection()) {
@@ -38,7 +43,8 @@ public class mp3toDB {
             st.setString(2, id3v2Tag.getArtist());
             st.setInt(3, Math.toIntExact(mp3file.getLengthInSeconds()));
             st.setString(4, id3v2Tag.getGenreDescription());
-
+            st.setString(5, "Data/"+file.getName());
+            
             int rowsAffected = st.executeUpdate();
 
             ResultSet rs = st.getGeneratedKeys();
